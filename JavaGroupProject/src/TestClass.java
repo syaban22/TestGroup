4d9878ae2b2e389233186ec800292021e1ff32f5
Syaban22 <syabansim@gmail.com>
2018-11-17 15:02:20 +0700

    perubahan dari admin gaes

--- JavaGroupProject/src/TestClass.java
+++ JavaGroupProject/src/TestClass.java
@@ -3,5 +3,6 @@ public class TestClass {
 	public static void main() {
 		System.out.println("This is a test");
 		System.out.println("This is changed by user 2");
+		System.out.println("This is changed by administrator");
 	}
 }

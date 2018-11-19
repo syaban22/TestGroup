1745127ce56f0ea0b3a602162b418818b97d58d8
Syaban <syabansim@gmail.com>
2018-11-19 13:40:23 +0700

    Code Master Project

---
+++ JavaFXApplication1/src/javafxapplication1/JavaFXApplication1.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javafxapplication1;
+
+import javafx.application.Application;
+import javafx.scene.*;
+import javafx.scene.control.*;
+import javafx.scene.layout.*;
+import javafx.stage.*;
+import javafx.geometry.*;
+
+/**
+ *
+ * @author Syaban22
+ */
+public class JavaFXApplication1 extends Application {
+    
+    @Override
+    public void start(Stage stage) {
+        Layout1(stage);
+    }
+    public void Layout1(Stage stage){
+    
+    }
+    
+    public void Layout2(Stage stage){
+    
+    }
+    
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+}
